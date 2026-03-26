b9887f804d2a10c39a5bcec28ac70668a641f694
nadiaGueta <segma55@outlook.com>
2026-03-26 16:15:16 +0100

    US-01 Compte thread-safe + US-04 Virement sans deadlock (identityHashCode)

--- src/banque/Main.java
+++ src/banque/Main.java
@@ -2,11 +2,42 @@ package banque;
 
 import banque.core.Banque;
 
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Projet OK");
 
-        Banque banque = new Banque();
-        banque.demarrer();
+import banque.model.Compte;
+import banque.core.Banque;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Compte c1 = new Compte(1, 10);
+        Compte c2 = new Compte(2, 1000);
+
+        //  A → B
+        Thread t1 = new Thread(() -> {
+            for (int i = 0; i < 10; i++) {
+                Banque.virer(c1, c2, 10);
+            }
+        });
+
+        //  B → A
+        Thread t2 = new Thread(() -> {
+            for (int i = 0; i < 10; i++) {
+                Banque.virer(c2, c1, 10);
+            }
+        });
+
+        t1.start();
+        t2.start();
+
+        try {
+            t1.join();
+            t2.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("Solde final c1 = " + c1.getSolde());
+        System.out.println("Solde final c2 = " + c2.getSolde());
     }
 }
--- src/banque/model/Compte.java
+++ src/banque/model/Compte.java
@@ -4,19 +4,39 @@ public class Compte {
     private final int numero;
     private double solde;
 
+    public int getNumero() {
+        return numero;
+    }
+
     public Compte(int numero, double soldeInitial) {
         this.numero = numero;
         this.solde = soldeInitial;
     }
 
     public synchronized void deposer(double montant) {
+        if ( montant <=0){
+
+            throw new IllegalArgumentException("Montant invalide ");
+        }
         solde += montant;   // TODO compléter
+        System.out.println("compte"+numero+"+"+montant+"solde:"+solde);
     }
 
     public synchronized boolean retirer(double montant) {
-        if (solde >= montant) { solde -= montant; return true; }
+        if(montant<=0){
+
+            throw new IllegalArgumentException("Montant invalide");
+        }
+
+
+        if (solde >= montant) {
+            solde -= montant;
+            System.out.println("Compte"+numero+"-"+montant+"=>solde:"+solde);
+            return true;
+        }
         return false;
     }
 
-    public synchronized double getSolde() { return solde; }
+    public synchronized double getSolde() {
+        return solde; }
 }
